39c6f613db10a848e1a3e40d8cfbb1a99a7dfc77
jhojnas <naxinn18@gmail.com>
2016-01-13 17:57:16 -0500

    sdf

---
+++ src/java/com/sistemas/educatic/rest/GenericResource.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sistemas.educatic.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Abel
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.sistemas.educatic.rest.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("prueba")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getXml() {
+        //TODO return proper representation object
+        return "hola";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+}
